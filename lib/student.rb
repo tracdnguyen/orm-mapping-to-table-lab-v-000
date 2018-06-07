8f9f27dd8f572ea35443d096c062e0eb34d47386
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-07 23:35:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,4 +20,11 @@ class Student
            SQL
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students;
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
