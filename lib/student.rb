5e0d1b999a1494666823efc5fb66725ab4638760
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-07 23:29:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,9 +12,11 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE students (
-
-    )
-    SQL
+    CREATE TABLE IF NOT EXISTS students (
+           id INTEGER PRIMARY KEY, 
+           name TEXT, 
+           grade INTEGER
+           )
+           SQL
   end
 end
